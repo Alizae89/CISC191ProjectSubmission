f853823df47628825ce9a430cebf92b44251838f
Alizae <alizaezaidi@gmail.com>
2025-04-26 17:09:48 -0700

    Work in Progress:
    I have started the layout of the project by implementing the user data
    for budgeting app.
     * This will be similar like any type of banking app that lets users add
    data to view and examine
     * their monthly spending on their expenses alongside their
    monthly/weekly wages.

---
+++ src/myCISC191project/ExpenseIncomeUserData.java
@@ -0,0 +1,66 @@
+/**
+* Lead Author(s):
+* @author alizae; student ID
+* @author Full name; student ID
+* <<Add additional lead authors here>>
+*
+* Other Contributors:
+* Full name; student ID or contact information if not in class
+* <<Add additional contributors (mentors, tutors, friends) here, with contact information>>
+*
+* References:
+* Morelli, R., & Walde, R. (2016).
+* Java, Java, Java: Object-Oriented Problem Solving
+* https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+*
+* <<Add more references here>>
+*
+* Version: 2025-04-26
+*/
+package myCISC191project;
+
+/**
+ * Purpose: The reponsibility of ExpenseIncomeUserData is ...
+ *
+ * ExpenseIncomeUserData is-a ...
+ * ExpenseIncomeUserData is ...
+ */
+public class ExpenseIncomeUserData
+{
+	private String date;
+	private String description;
+	private Double transaction;
+	private String type;
+	
+	
+	public ExpenseIncomeUserData(String date, String description, double transaction, String type)
+	{
+		this.date = date;
+		this.description = description;
+		this.transaction = transaction;
+		this.type = type;
+	}
+	public String retrieveDate()
+	{
+		return date;
+
+	}
+	
+	public String retrieveDescription()
+	{
+		return description;
+
+	}
+
+	public double retrieveTransaction()
+	{
+		return transaction;
+	}
+
+	public String retrieveType() 
+	{
+		return type;
+	}
+	
+
+}
